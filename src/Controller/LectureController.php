b94f505a50f0c59632d6075a6531471bd67d557d
Julien Collard <juliencollard2001@gmail.com>
2023-01-23 16:16:38 +0100

    COMMIT de JULIEN LE 23/01

--- src/Controller/LectureController.php
+++ src/Controller/LectureController.php
@@ -18,38 +18,41 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/lecture", name: "lecture_")]
-class LectureController extends AbstractController
-{
-    #[Route("/index", name: "index")]
-    public function lectureHome(): Response
-    {
+
+class LectureController extends AbstractController {
+#[Route("/index", name: "index")]
+
+    public function lectureHome(): Response {
         return $this->render("lecture/index.html.twig");
     }
 
-    #[Route("/form", name: "form")]
+#[Route("/form", name: "form")]
+
     public function form(
-        SessionRepository        $session_repository,
-        NiveauScolaireRepository $niveau_scolaire_repository,
-        Request                  $request,
-        EntityManagerInterface   $entity_manager,
-    ): Response
-    {
+            SessionRepository $session_repository,
+            NiveauScolaireRepository $niveau_scolaire_repository,
+            Request $request,
+            EntityManagerInterface $entity_manager
+    ): Response {
+
         $reponse = new ReponseCandidat(
-            id: 0,
-            session: $session_repository->findOneBy([]),
-            reponses: array(),
-            nom: "",
-            prenom: "",
-            nom_jeune_fille: "",
-            niveau_scolaire: $niveau_scolaire_repository->findOneBy([]),
-            date_de_naissance: new DateTime("now"),
-            sexe: 1,
-            reserve: "",
-            autre_1: "",
-            autre_2: "",
-            code_barre: 0,
-            raw: null
+                0,
+                $session_repository->findOneBy([]),
+                array(),
+                "",
+                "",
+                "",
+                $niveau_scolaire_repository->findOneBy([]),
+                new DateTime("now"),
+                1,
+                "",
+                "",
+                "",
+                0,
+                null
         );
+
+
 
         $form = $this->createForm(ReponseCandidatType::class, $reponse);
 
@@ -61,19 +64,17 @@ class LectureController extends AbstractController
             $entity_manager->flush();
 
             return $this->redirectToRoute("session_consulter", ["id" => $reponse->session->id]);
-
         }
 
         return $this->render("lecture/from_form.html.twig", ["form" => $form]);
     }
 
-    #[
-        Route("/fichier", name: 'fichier')]
-    public function fichier(ManagerRegistry          $doctrine,
-                            NiveauScolaireRepository $niveau_scolaire_repository,
-                            Request                  $request,
-                            LoggerInterface          $logger): Response
-    {
+#[Route("/fichier", name: 'fichier')]
+
+    public function fichier(ManagerRegistry $doctrine,
+            NiveauScolaireRepository $niveau_scolaire_repository,
+            Request $request,
+            LoggerInterface $logger): Response {
         $manager = $doctrine->getManager();
 
         $uploadSessionBase = new ParametresLectureJSON();
@@ -97,20 +98,20 @@ class LectureController extends AbstractController
                 dump($reponse_array);
 
                 $reponse_candidat = new ReponseCandidat(
-                    id: 0,
-                    session: $uploadSessionBase->session,
-                    reponses: $reponse_array,
-                    nom: $reponses_candidat_json["nom"],
-                    prenom: $reponses_candidat_json["prenom"],
-                    nom_jeune_fille: $reponses_candidat_json["nom_jeune_fille"],
-                    niveau_scolaire: $niveau_scolaire_repository->findOneBy([]),
-                    date_de_naissance: new DateTime("now"),
-                    sexe: $reponses_candidat_json["sexe"],
-                    reserve: $reponses_candidat_json["reserve"],
-                    autre_1: $reponses_candidat_json["autre_1"],
-                    autre_2: $reponses_candidat_json["autre_2"],
-                    code_barre: $reponses_candidat_json["code_barre"],
-                    raw: $reponses_candidat_json
+                        0,
+                        $uploadSessionBase->session,
+                        $reponse_array,
+                        $reponses_candidat_json["nom"],
+                        $reponses_candidat_json["prenom"],
+                        $reponses_candidat_json["nom_jeune_fille"],
+                        $niveau_scolaire_repository->findOneBy([]),
+                        new DateTime("now"),
+                        $reponses_candidat_json["sexe"],
+                        $reponses_candidat_json["reserve"],
+                        $reponses_candidat_json["autre_1"],
+                        $reponses_candidat_json["autre_2"],
+                        $reponses_candidat_json["code_barre"],
+                        $reponses_candidat_json
                 );
                 $manager->persist($reponse_candidat);
             }
@@ -121,34 +122,19 @@ class LectureController extends AbstractController
         }
 
         return $this->render('lecture/from_file.html.twig', [
-            'form' => $form->createView(),
+                    'form' => $form->createView(),
         ]);
     }
 
-    #[Route("/scanner", name: "scanner")]
-    public function scanner(): Response
-    {
-        // Spécifier le port. A priori, il faut un ":" à la fin. On trouve le numéro du port grâce à la ligne de commande "mode" dans le terminal
-        $fd = dio_open("COM3:", O_RDWR);
+#[Route("/scanner", name: "scanner")]
 
-        // Normalement, c'est les bons paramètres
-        dio_tcsetattr($fd, array(
-            'baud' => 19200,
-            'bits' => 8,
-            'stop' => 1,
-            'parity' => 0
-        ));
-
-        // https://www.php.net/manual/en/function.dio-write.php
-        dio_write($fd, data: "v\n", len: 1); // ecrit 1 bit de la chaine de caractère data
-
-        // https://www.php.net/manual/en/function.dio-read.php
-        $x = dio_read($fd, 1); // Lit 1 bit
-
-        dump($x); // dump est pas mal pour le debug, c'est mis en forme par symfony profiler (quand c'est pas juste un nombre)
-
-        dio_close($fd);
-        return $this->render("lecture/from_scanner.html.twig", ["form" => null]);
+    public function scanner(): Response {
+        //si pas de session spécifiée, la demander !
+        //si la session est renseignée : ($session disponible)
+        //ancienne vue
+        //return $this->render("lecture/from_scanner.html.twig", ["form" => null]);
+        //nouvelle vue
+        return $this->render("lecture/lecteur_optique.html.twig", ["form" => null]);
     }
 
 }
