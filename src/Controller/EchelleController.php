3e92198eb5adc18585502c56d930d4ea483ff2f6
jbmoussa <jb.moussa98@gmail.com>
2023-02-02 17:33:14 +0900

    Creation d'echelle et ajout d'echelle au correcteur

--- src/Controller/EchelleController.php
+++ src/Controller/EchelleController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Echelle;
 use App\Form\EchelleType;
 use App\Repository\EchelleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,6 +30,32 @@ class EchelleController extends AbstractCrudController
             nom: "",
             nom_php: "", type: Echelle::TYPE_ECHELLE_SIMPLE
         );
+    }
+
+    #[Route("/creation/{id}", name: "creation")]
+    public function creation(Request $request,
+                             EntityManagerInterface $entityManager,
+                             int $id,
+    ): Response
+    {
+        $echelle = $this->produce();
+
+        $form = $this->createForm(EchelleType::class, $echelle);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+
+            $echelle = $form->getData();
+            $entityManager->persist($echelle);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('correcteur_ajoutEchelle', ['id' => $id]);
+        }
+
+        return $this->render('echelle/creer.html.twig', [
+            'form' => $form,
+        ]);
+
     }
 
     protected function renderIndex(array $items): Response
