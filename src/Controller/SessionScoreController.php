196f62dd958de898f7eebe260137ee93f589cd0e
Jb Moussa <jb.moussa98@gmail.com>
2023-04-19 09:00:38 +0200

    MAJ calcul scores recherche

--- src/Controller/SessionScoreController.php
+++ src/Controller/SessionScoreController.php
@@ -9,7 +9,9 @@ use App\Core\Files\Csv\CsvScoreManager;
 use App\Entity\Session;
 use App\Form\CorrecteurChoiceType;
 use App\Form\Data\CorrecteurChoice;
+use App\Recherche\ReponsesCandidatSessionStorage;
 use App\Repository\CorrecteurRepository;
+use App\Repository\ReponseCandidatRepository;
 use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -90,5 +92,82 @@ class SessionScoreController extends AbstractController
         $scores = $correcteur_manager->corriger($correcteur, $reponses);
 
         return $csv_score_manager->export($session, $correcteur->profil, $scores);
+    }
+
+    #[Route('/recherche/form/correcteur/{session_id}', name: "recherche_form_correcteur")]
+    public function rechercheFormCorrecteur(
+        SessionRepository $session_repository,
+        Request           $request,
+        int               $session_id): Response
+    {
+        /** @var Session $session */
+        $session = $session_repository->find($session_id);
+
+        $parametres_calcul_score = new CorrecteurChoice();
+        $form = $this->createForm(CorrecteurChoiceType::class,
+            $parametres_calcul_score,
+            [CorrecteurChoiceType::OPTION_SESSION => $session]
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $correcteur = $parametres_calcul_score->correcteur;
+
+            return $this->redirectToRoute("calcul_score_recherche_score",
+                ["session_id" => $session_id, "correcteur_id" => $correcteur->id]);
+        }
+
+        return $this->render('score/form.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route("/recherche/score/{session_id}/{correcteur_id}", name: "recherche_score")]
+    public function consulter_score(
+        SessionRepository    $session_repository,
+        CorrecteurRepository $correcteur_repository,
+        CorrecteurManager    $correcteur_manager,
+        int                  $session_id,
+        int                  $correcteur_id,
+        ReponsesCandidatSessionStorage $reponses_candidat_session_storage,
+        ReponseCandidatRepository      $reponse_candidat_repository,
+    ): Response
+    {
+        $cached_reponses_ids = $reponses_candidat_session_storage->get();
+        $reponses = $reponse_candidat_repository->findAllByIds($cached_reponses_ids);
+
+        $session = $session_repository->find($session_id);
+        $correcteur = $correcteur_repository->find($correcteur_id);
+
+        $scores = $correcteur_manager->corriger($correcteur, $reponses);
+
+        return $this->render("recherche/score_index.html.twig",
+            ["scores" => $scores,
+                "session" => $session,
+                "correcteur" => $correcteur,
+                "reponses" => $reponses]);
+    }
+    #[Route("/recherche/score/csv/{session_id}/{correcteur_id}", name: "calcul_score_recherche_score_csv")]
+    public function score_csv(
+        SessionRepository    $session_repository,
+        CorrecteurRepository $correcteur_repository,
+        CorrecteurManager    $correcteur_manager,
+        CsvScoreManager      $csv_score_manager,
+        ReponsesCandidatSessionStorage $reponses_candidat_session_storage,
+        ReponseCandidatRepository      $reponse_candidat_repository,
+        int                  $session_id,
+        int                  $correcteur_id,
+    ): Response
+    {
+        $cached_reponses_ids = $reponses_candidat_session_storage->get();
+        $reponses = $reponse_candidat_repository->findAllByIds($cached_reponses_ids);
+
+        $session = $session_repository->find($session_id);
+        $correcteur = $correcteur_repository->find($correcteur_id);
+
+        $scores = $correcteur_manager->corriger($correcteur, $reponses);
+
+        return $csv_score_manager->export($session, $correcteur->profil, $scores, $reponses);
     }
 }
